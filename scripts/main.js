87f9608b260abb5296ec2f97e7c137865e11a1e3
dnc <sewaiper@bk.ru>
2020-11-26 19:58:04 +0300

    Added base game logic

--- scripts/main.js
+++ scripts/main.js
@@ -5,23 +5,99 @@ let width = canvas.width;
 let height = canvas.height;
 
 context.fillStyle = "rgb(255, 255, 255, 1)";
-let coeff = 25;
-let x = 0;
-let dir = 1;
+let active = false;
+let speed = 0;
+
+let diff = getDiff(width, height, 30);
+
+let motion = {
+    dx: diff,
+    dy: 0
+}
+
+let pixel = {
+    x: 0,
+    y: 0
+}
+
+function getDiff(width, height, coeff) {
+    let diff = 0;
+    
+    if(width > height) {
+        diff = height/coeff;
+    }
+    else {
+        diff = width/coeff;
+    }
+    
+    return diff;
+}
 
 function draw() {
     window.requestAnimationFrame(draw);
-    x += dir;
     
-    if((x+width/coeff) >= width) dir = -1;
-    if(x == 0) dir = 1;
+    if(!active) return;
     
-    context.clearRect(0, 0, width, height);
-    
-    context.beginPath();
-    context.moveTo(x, 0);
-    context.lineTo(x, height/coeff);
-    context.lineTo(x+width/coeff, (height/coeff)/2);
-    context.lineTo(x, 0);
-    context.fill();
+    if(speed > 0) {
+        speed--;
+    }
+    else {
+        speed = 5;
+        
+        pixel.x += motion.dx;
+        pixel.y += motion.dy;
+
+        if(pixel.x >= width) pixel.x = -diff;
+        if(pixel.y >= height) pixel.y = -diff;
+
+        if(pixel.x < (-diff)) pixel.x = width;
+        if(pixel.y < (-diff)) pixel.y = height;
+
+        context.clearRect(0, 0, width, height);
+
+        context.fillRect(pixel.x, pixel.y, diff, diff);
+        
+        console.log(pixel.x);
+        console.log(pixel.y);
+    }
 }
+
+function event(event) {
+    switch(event.key) {
+        case "ArrowUp":
+            if(pixel.x >= 0 && (pixel.x+diff) <= width) {
+                motion.dx = 0;
+                motion.dy = -diff;
+            }
+        break;
+        case "ArrowDown":
+            if(pixel.x >= 0 && (pixel.x+diff) <= width) {
+                motion.dx = 0;
+                motion.dy = diff;
+            }
+        break;
+        case "ArrowLeft":
+            if(pixel.y >= 0 && (pixel.y+diff) <= height) {
+                motion.dx = -diff;
+                motion.dy = 0;
+            }
+        break;
+        case "ArrowRight":
+            if(pixel.y >= 0 && (pixel.y+diff) <= height) {
+                motion.dx = diff;
+                motion.dy = 0;
+            }
+        break;
+    }
+}
+
+canvas.addEventListener("keydown", event, true);
+canvas.onfocus = function() {
+    active = true;
+}
+canvas.onblur = function() {
+    active = false;
+}
+
+canvas.focus();
+draw();
